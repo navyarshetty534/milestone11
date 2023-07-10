46b4a56f49e3dd6af6a868a5f152a3da3a10c9ed
navyarshetty534 <139110961+navyarshetty534@users.noreply.github.com>
2023-07-10 16:23:00 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css"
+import Home from './Components/Home'
+import Menu from './Components/Menu'
+import About from './Components/About'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Navbar from './Components/Navbar'
+const App = () => {
+  return (
+    <div>
+     <BrowserRouter>
+     <Navbar></Navbar>
+     <Routes>
+<Route path='/' element={<Home></Home>}></Route>
+<Route path='/about' element={<About></About>}></Route>
+<Route path='/menu' element={<Menu></Menu>}></Route>
+     </Routes>
+     </BrowserRouter>      
+    </div>
+  )
+}
+
+export default App
+
